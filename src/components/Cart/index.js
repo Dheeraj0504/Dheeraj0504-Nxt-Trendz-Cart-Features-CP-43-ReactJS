922013ec9b85d0c28caaaa42ec82da814104cf0b
Dheeraj0504 <0129dheerajkumar@gmail.com>
2024-05-13 15:25:24 +0000

    Nxt Trendz - Application

---
+++ src/components/Cart/index.js
@@ -0,0 +1,49 @@
+import Header from '../Header'
+import CartListView from '../CartListView'
+
+import CartContext from '../../context/CartContext'
+import EmptyCartView from '../EmptyCartView'
+import CartSummary from '../CartSummary'
+
+import './index.css'
+
+const Cart = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList, removeAllCartItems} = value
+      const showEmptyView = cartList.length === 0
+
+      /** TODO: Update the functionality to remove all the items in the cart */
+      const onClickRemoveAllBtn = () => {
+        // console.log("Remove All Btn Clicked")
+        removeAllCartItems()
+      }
+
+      return (
+        <>
+          <Header />
+          <div className="cart-container">
+            {showEmptyView ? (
+              <EmptyCartView />
+            ) : (
+              <div className="cart-content-container">
+                <h1 className="cart-heading">My Cart</h1>
+                <button
+                  className="remove-all-btn"
+                  type="button"
+                  onClick={onClickRemoveAllBtn}
+                >
+                  Remove All
+                </button>
+                <CartListView />
+                {/* TODO: Add your code for Cart Summary here */}
+                <CartSummary />
+              </div>
+            )}
+          </div>
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default Cart
